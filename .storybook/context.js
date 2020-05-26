fe6acfa0f9a037e2b4647fe5371a50471a385084
Alexander Georgiev <alexfromsofia@gmail.com>
2020-05-26 16:53:04 +0300

    Add storybook

---
+++ .storybook/context.js
@@ -0,0 +1,15 @@
+import { ThemeProvider } from "styled-components";
+
+import { defaultTheme, darkTheme } from "../src/utils";
+
+export const contexts = [
+  {
+    icon: "box",
+    title: "Themes",
+    components: [ThemeProvider],
+    params: [
+      { name: "Default theme", props: { theme: defaultTheme }, default: true },
+      { name: "Dark theme", props: { theme: darkTheme }, default: true }
+    ]
+  }
+];
